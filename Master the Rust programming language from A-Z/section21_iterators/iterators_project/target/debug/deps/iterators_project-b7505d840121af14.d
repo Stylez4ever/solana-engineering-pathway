ec53740a9a039bfabb09b3def43b8a0ad1283083
Stylez4ever <129878977+Stylez4ever@users.noreply.github.com>
2026-04-08 01:09:49 +0800

    updates from main repo

--- Master the Rust programming language from A-Z/section21_iterators/iterators_project/target/debug/deps/iterators_project-b7505d840121af14.d
+++ Master the Rust programming language from A-Z/section21_iterators/iterators_project/target/debug/deps/iterators_project-b7505d840121af14.d
@@ -1,6 +1,6 @@
-C:\Users\masha\solana-engineering-pathway\Master the Rust programming language from A-Z\section21_iterators\iterators_project\target\debug\deps\iterators_project-b7505d840121af14.d: src\lib.rs src\p1_manual_iteration.rs src\p2_into_iterator_trait.rs src\p3_exhausting_the_iterator.rs src\p4_for_loop_with_iterator.rs src\p5_iter_method.rs src\p6_iter_mut_method.rs src\p7_hashmap_iteration.rs src\p8_string_iteration.rs src\p9_solving_problems_with_iteration.rs src\p10_for_each_method.rs src\p11_the_map_method.rs src\p12_the_collect_method.rs src\p13_the_map_method_2.rs
+C:\Users\masha\solana-engineering-pathway\Master the Rust programming language from A-Z\section21_iterators\iterators_project\target\debug\deps\iterators_project-b7505d840121af14.d: src\lib.rs src\p1_manual_iteration.rs src\p2_into_iterator_trait.rs src\p3_exhausting_the_iterator.rs src\p4_for_loop_with_iterator.rs src\p5_iter_method.rs src\p6_iter_mut_method.rs src\p7_hashmap_iteration.rs src\p8_string_iteration.rs src\p9_solving_problems_with_iteration.rs src\p10_for_each_method.rs src\p11_the_map_method.rs src\p12_the_collect_method.rs src\p13_the_map_method_2.rs src\p14_the_filter_and_find_methods.rs src\p15_the_filter_and_find_methods_2.rs
 
-C:\Users\masha\solana-engineering-pathway\Master the Rust programming language from A-Z\section21_iterators\iterators_project\target\debug\deps\libiterators_project-b7505d840121af14.rmeta: src\lib.rs src\p1_manual_iteration.rs src\p2_into_iterator_trait.rs src\p3_exhausting_the_iterator.rs src\p4_for_loop_with_iterator.rs src\p5_iter_method.rs src\p6_iter_mut_method.rs src\p7_hashmap_iteration.rs src\p8_string_iteration.rs src\p9_solving_problems_with_iteration.rs src\p10_for_each_method.rs src\p11_the_map_method.rs src\p12_the_collect_method.rs src\p13_the_map_method_2.rs
+C:\Users\masha\solana-engineering-pathway\Master the Rust programming language from A-Z\section21_iterators\iterators_project\target\debug\deps\libiterators_project-b7505d840121af14.rmeta: src\lib.rs src\p1_manual_iteration.rs src\p2_into_iterator_trait.rs src\p3_exhausting_the_iterator.rs src\p4_for_loop_with_iterator.rs src\p5_iter_method.rs src\p6_iter_mut_method.rs src\p7_hashmap_iteration.rs src\p8_string_iteration.rs src\p9_solving_problems_with_iteration.rs src\p10_for_each_method.rs src\p11_the_map_method.rs src\p12_the_collect_method.rs src\p13_the_map_method_2.rs src\p14_the_filter_and_find_methods.rs src\p15_the_filter_and_find_methods_2.rs
 
 src\lib.rs:
 src\p1_manual_iteration.rs:
@@ -16,3 +16,5 @@ src\p10_for_each_method.rs:
 src\p11_the_map_method.rs:
 src\p12_the_collect_method.rs:
 src\p13_the_map_method_2.rs:
+src\p14_the_filter_and_find_methods.rs:
+src\p15_the_filter_and_find_methods_2.rs:
