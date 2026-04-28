fe900c5d0a6e86d46c446c3b3d54d4517fea330a
Tyson Masha <129878977+Stylez4ever@users.noreply.github.com>
2026-04-28 06:05:39 +0000

    Section 26 exercise update_3

---
+++ Master the Rust programming language from A-Z/section26_smart_pointers/src/p7_deref_and_derefmut_traits.rs
@@ -0,0 +1,39 @@
+use std::ops::{Deref, DerefMut};
+struct CustomBox<T> {
+    data: T,
+}
+
+impl<T> CustomBox<T> {
+    fn new(data: T) -> Self {
+        Self {
+            data,
+        }
+    }
+}
+
+impl<T> Deref for CustomBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+
+    
+}
+
+impl<T> DerefMut for CustomBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
+    }
+}
+
+fn main() {
+    let mut boxy = Box::new(3.14);
+    *boxy = 6.42;
+    println!("{}", *boxy);
+
+    let mut custom_boxy = CustomBox::new(3.14);
+    *custom_boxy =3.46;
+    println!("{}", *custom_boxy);
+
+}
